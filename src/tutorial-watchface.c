3332e9c0a73c9a4a6ebc1114f8a96005c63306da
Wiggins <wiggins@concentricsky.com>
2013-05-09 10:39:01 -0700

    The basic watchface project.

--- src/tutorial-watchface.c
+++ src/tutorial-watchface.c
@@ -5,25 +5,42 @@
 
 #define MY_UUID { 0xBD, 0x46, 0x1D, 0x5E, 0xCA, 0x85, 0x40, 0xF3, 0xB3, 0x63, 0x15, 0x99, 0xA1, 0x42, 0x29, 0x8F }
 PBL_APP_INFO(MY_UUID,
-             "Template App", "Your Company",
+             "TemplateWatch", "Concentric Sky",
              1, 0, /* App version */
-             DEFAULT_MENU_ICON,
+             RESOURCE_ID_IMAGE_MENU_ICON,
              APP_INFO_STANDARD_APP);
 
-Window window;
+
+Window g_Window;
+
+
+void handle_second_tick(AppContextRef ctx, PebbleTickEvent *t) {
+  (void)ctx;
+  (void)t;
+
+}
 
 
 void handle_init(AppContextRef ctx) {
   (void)ctx;
 
-  window_init(&window, "Window Name");
-  window_stack_push(&window, true /* Animated */);
+  /*
+   * initialize the window
+   */
+  window_init(&g_Window, "TemplateWatch");
+  window_stack_push(&g_Window, true /* Animated */);
+  window_set_background_color(&g_Window, GColorBlack);
+  resource_init_current_app(&APP_RESOURCES);
 }
 
 
 void pbl_main(void *params) {
   PebbleAppHandlers handlers = {
-    .init_handler = &handle_init
+    .init_handler = &handle_init,
+    .tick_info = {
+      .tick_handler = &handle_second_tick,
+      .tick_units = SECOND_UNIT
+    }
   };
   app_event_loop(params, &handlers);
 }
